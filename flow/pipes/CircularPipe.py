574d846276b14f9318ab3aa9cccd889813329ba4
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-12-17 11:27:29 -0800

    Started filling in pipe properties.

--- flow/pipes/CircularPipe.py
+++ flow/pipes/CircularPipe.py
@@ -1,6 +1,25 @@
+from __future__ import division, print_function, absolute_import
+from math import pi
 from .Pipe import Pipe
 
 class CircularPipe(Pipe):
 
     def __init__(self, radius, length):
+        self._radius = radius
+        self._length = length
+
+    def velocity(self, radius=0, pressure=None, fluid=None):
         pass
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @property
+    def length(self):
+        return self.length
+
+    @property
+    def section(self):
+        return pi * self.radius**2.
+
--- flow/pipes/Pipe.py
+++ flow/pipes/Pipe.py
@@ -1,7 +1,11 @@
-
+from __future__ import division, print_function, absolute_import
+from .. import ur
 
 
 class Pipe(object):
+
+    _fluid = None
+    _pressure = None
 
     def __init__(self):
         pass
@@ -12,17 +16,32 @@ class Pipe(object):
     def __repr__(self):
         pass
 
+    def fluid(self, fluid=False):
+        if fluid is not False:
+            self._fluid = fluid
+        else:
+            return self._fluid
+
+    def pressure(self, pressure=False):
+        if pressure is not False:
+            self._pressure = pressure
+        else:
+            return self._pressure
+
     def flowrate(self, pressure, viscosity=None):
         raise NotImplementedError
 
     def resistance(self, viscosity=None):
         raise NotImplementedError
 
+    @property
     def volume(self):
-        raise NotImplementedError
+        return self.section * self.length
 
+    @property
     def section(self):
         raise NotImplementedError
 
+    @property
     def surface(self):
         raise NotImplementedError
