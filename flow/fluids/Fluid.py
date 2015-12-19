645c9b584c96f00a1cc0ce2f583e54ef44d56d33
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-12-18 20:42:32 -0800

    Draft of unit conversion decorator for Fluid.

--- flow/fluids/Fluid.py
+++ flow/fluids/Fluid.py
@@ -1,6 +1,11 @@
 from __future__ import print_function, division, absolute_import
+from .. import ur, Q
 
 class Fluid(object):
+
+    _default_units = {'temperature': ur.degC,
+                      'pressure': ur.bar,
+                      }
 
     @classmethod
     def viscosity(self, temperature=None, pressure=None):
@@ -23,3 +28,26 @@ class Fluid(object):
     @classmethod
     def __repr__(self):
         pass
+
+    @classmethod
+    def _wrap(self, f):
+        """Decorator to check units on temperature and pressure."""
+        def wrapper(self, temperature=None, pressure=None):
+            temperature = Q(temperature)
+            pressure = Q(pressure)
+
+            if temperature.dimensionless:
+                temperature *= self._default_units['temperature']
+            elif temperature.dimensionality != \
+                    self._default_units['temperature'].dimensionality:
+                raise RuntimeError('Incompatible units.')
+
+            if pressure.dimensionless:
+                pressure *= self._default_units['pressure']
+            elif pressure.dimensionality != \
+                    self._default_units['pressure'].dimensionality:
+                string = 'Incompatible units. {}.dimensionality != {}.dimensonality'
+                raise RuntimeError(string.format(pressure,
+                                                 self._default_units['pressure']
+                                                 )
+
