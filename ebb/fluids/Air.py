53b9a17566df3926ffafb7684564de703f193f34
Lewis A. Marshall <lewis@purigenbio.com>
2017-02-14 20:41:03 -0800

    Added sutherlands formula for gas viscosity.

--- ebb/fluids/Air.py
+++ ebb/fluids/Air.py
@@ -5,6 +5,9 @@ from ..units import unit_registry as ur, Quantity as Q
 class Air(Gas):
 
     molecular_weight = Q('29 grams/mol')
+    reference_viscosity = Q('18.27 uPa s')
+    reference_temperature = Q('291.15 K')
+    sutherland = Q('120 K')
 
     @classmethod
     def viscosity(self, temperature=None, pressure=None):
--- ebb/fluids/Gas.py
+++ ebb/fluids/Gas.py
@@ -5,12 +5,21 @@ from ..constants import gas_constant
 from ..units import Quantity as Q
 
 class Gas(Fluid):
+
     molecular_weight = Q('0 gram/mol')
+    reference_viscosity = Q('0 uPa s')
+    reference_temperature = Q('0 K')
+    sutherland = Q('0 K')
 
     @classmethod
     def _viscosity(self, temperature, pressure):
-        return 1
+        """Estimate the viscosity using Sutherland's formula."""
+        viscosity = (temperature / self.reference_temperature)**(3/2)
+        viscosity *= ((self.reference_temperature - self.sutherland) /
+                     (temperature - self.sutherland))
+        return viscosity * self.reference_viscosity
 
     @classmethod
     def _density(self, temperature, pressure):
+        """Estimate the density using the ideal gas law."""
         return pressure * self.molecular_weight/gas_constant/temperature.to('kelvin')
